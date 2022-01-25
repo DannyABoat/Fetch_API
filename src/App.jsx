6a77de049264bedbe478a900829ecffa229b436f
DannyABoat <dannysum07@yahoo.com>
2022-01-25 18:23:46 +0000

    included a useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
 import { useState, useEffect } from "react"
-import './App.css'
+import "./App.css"
 
 // https://cat-fact.herokuapp.com/facts/random
 
 const App = () => {
-    const [fact, setFact] = useState({})
-    const [data, setData] = useState({})
+  const [fact, setFact] = useState({})
+  const [data, setData] = useState({})
 
+  // equivalent to componentDidMount
+  useEffect(() => {
+    handleFetch()
+  }, [])
 
-    // equivalent to componentDidMount
-    useEffect(() => {
-        handleFetch()
-    }, [])
-
-
-      // equivalent to componentDidUpdate
+  //   equivalent to componentDidUpdate
   //   but with specification
-    // useEffect(() => {
-    //     console.log("The fact has changed")
-    // }, [fact])
+  useEffect(() => {
+    console.log("The fact has changed")
+  }, [fact])
 
-    // useEffect(() => {
-    //     console.log("Component did update")
-    // })
+  useEffect(() => {
+    console.log("component did update")
+  })
 
-    const handleFetch = async () => {
-        const response = await fetch("https://cat-fact.herokuapp.com/facts/random")
-        const data = await response.json()
-        console.log(data)
+  const handleFetch = async () => {
+    const res = await fetch("https://cat-fact.herokuapp.com/facts/random")
+    const data = await res.json()
 
-        setFact(data)
-        
-    }
+    setFact(data)
+  }
 
-
-    return(
-        <div>
-            <h1>Hello API's!</h1>
-            <button onClick={handleFetch}> Run the fetch request!</button>
-            <button onClick={() => setData("Hi")}>Add data</button>
-            <p>{fact.test}</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello API's!</h1>
+      <button onClick={handleFetch}>Run the fetch request!</button>
+      <button onClick={() => setData("hi")}>Add data</button>
+      <p>{fact.text}</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
